16ebe14242b5e9e9691184766c748b9a4f7917b1
UzakoffUmar <uzakovumar338@gmail.com>
2023-01-20 22:14:40 +0500

    sssorry

---
+++ script.js
@@ -0,0 +1,11 @@
+var num = prompt('Ismingizning birinchi harfini kiriting)')
+
+if ((num) >= 0) {
+    alert(`Ismingiz Munisa bo'lsa, shunchaki tabassum qiling)`);
+}
+else if ((num) < 0) {
+    alert(`Ismingiz Munisa bo'lsa, shunchaki tabassum qiling)`);
+}
+else {
+    alert(`Ismingiz Munisa bo'lsa, shunchaki tabassum qiling)`);
+}
